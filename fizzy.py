3fc597093fb07f3b7e5bd68697babccd75086b50
linda877 <lh4205@g.rit.edu>
2025-09-25 09:04:38 +0400

    added my fizzy

---
+++ fizzy.py
@@ -0,0 +1,25 @@
+def fizzbuzz(num):
+    if num %3==0 and num%5==0:
+        print ("Fizzbuzz")
+        
+    elif num%3==0:
+        print("Fizz")
+        
+    elif num%5==0:
+        print("Buzz")
+        
+    else:
+        print(num)
+       
+def main():
+    user_input=int(input("enter a number: ")
+        if not user_input.isdigit():
+            print ("invalid input")
+            return 
+            
+ for i in range (1, num+1):
+     fizzbuzz(i)
+     
+     
+ 
+    
